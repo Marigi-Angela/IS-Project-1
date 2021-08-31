4e28723ab21f30c3fda4124cbbb7d2b1ab46190f
Marigi-Angela <68993554+Marigi-Angela@users.noreply.github.com>
2021-08-31 23:40:12 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-3">
+                <h4>Pages</h4>
+                <ul>
+                    <li><a href="cart.php"></a>shopping Cart</li>
+                    <li><a href="contact.php"></a>contact us</li>
+                    <li><a href="shop.php"></a>shop</li>
+                    <li><a href="checkout.php"></a>my account</li>
+                </ul>
+            </div></div></div></div>
+            
