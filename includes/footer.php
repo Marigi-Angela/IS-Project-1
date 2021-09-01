e09c892ec21bacf11bf72c5ec83aa1fab143a40d
Marigi-Angela <68993554+Marigi-Angela@users.noreply.github.com>
2021-09-01 12:04:25 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -9,5 +9,44 @@
                     <li><a href="shop.php"></a>shop</li>
                     <li><a href="checkout.php"></a>my account</li>
                 </ul>
-            </div></div></div></div>
-            
+                <hr>
+                <h4>User Section</h4>
+                <ul>
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="consumerregister.php">Register</a></li><!--changed customer_register.php to consumerregister-->
+                </ul>
+                <hr class="hidden-md hidden-lg hidden-sm">
+            </div>
+            <div class="com-sm-6 col-md-3">
+                <h4>Top Products Categories</h4>
+                <ul>
+                    <li><a href="#">fruits</a></li>
+                    <li><a href="#">vegetables</a></li>
+                    <li><a href="#">dairy products</a></li>
+                    <li><a href="#">honey</a></li>
+                </ul>
+
+                <hr class="hidden-md hidden-lg">
+            </div>
+
+            <hr>
+                <h4>keep in touch</h4>
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>
+
+        </div>
+    </div>
+</div>
+
+<div id="copyright">
+    <div class="container">
+        <div class="col-md-6">
+            <p class="pull-right">By: <a href="a">Angela</a></p>
+        </div>
+    </div>
+</div>
+     
